80f9d3c7fd03639181f68dcd7ecc66d20b65c8b7
abhipray-kr <abhipray.dumka@kroger.com>
2026-06-05 14:27:38 +0530

    fix: use auth.protect() directly in clerkMiddleware (Clerk v7 pattern)

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublic = createRouteMatcher(['/sign-in(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublic(req)) (await auth()).protect()
+  if (!isPublic(req)) await auth.protect()
 })
 
 export const config = { matcher: ['/((?!_next|.*\\..*).*)'] }
